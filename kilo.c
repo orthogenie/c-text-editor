1234c0fc960dabcfa411c0640adc48fe8b37af81
orthogenie <mrmabrur@outlook.com>
2025-10-08 22:19:38 +1100

    welcome message

--- kilo.c
+++ kilo.c
@@ -4,16 +4,20 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** DEFINES ***/
 
+#define KILO_VERSION "0.0.1"
+
 #define FAILURE -1
 #define SUCCESS 0
 
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define ABUF_INIT {NULL, 0}
 
 /*** DATA ***/
 
@@ -25,6 +29,11 @@ struct editorConfig {
 
 struct editorConfig E;
 
+struct abuf {
+	char* b;
+	int len;
+};
+
 /*** FUNCTIONS ***/
 
 void die(const char*);
@@ -35,11 +44,14 @@ void enableRawMode(void);
 char editorReadKey(void);
 void editorRefreshScreen(void);
 void editorProcessKeypress(void);
-void editorDrawRows(void);
+void editorDrawRows(struct abuf*);
 void initEditor(void);
 
 int getWindowSize(int*, int*);
 int getCursorPosition(int*, int*);
+
+void abAppend(struct abuf*, const char*, int);
+void abFree(struct abuf*);
 
 /*** TERMINAL HELPERS ***/
 
@@ -116,27 +128,75 @@ int getWindowSize(int* rows, int* cols) {
 	}
 }
 
+/*** APPEND BUFFER ***/
+
+/* Reassign memory for buffer. */
+void abAppend(struct abuf* ab, const char* s, int len) {
+	char* new = realloc(ab->b, ab->len + len);
+
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+
+/* Free buffer memory. */
+void abFree(struct abuf* ab) {
+	free(ab->b);
+}
+
 /*** OUTPUT ***/
 
-void editorDrawRows(void) {
+/* Draw each row for each row in the terminal screen. */
+void editorDrawRows(struct abuf* ab) {
 	int y;
 
 	for (y = 0; y < E.screenrows; y++) {
-		write(STDOUT_FILENO, "~", 1);
+		if (y == E.screenrows / 3) { // First line welcome message
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome),
+				"Kilo editor -- version %s", KILO_VERSION);
+			
+			if (welcomelen > E.screencols) welcomelen = E.screencols; // Truncate
 
+			// Centre the welcome message
+			int padding = (E.screencols - welcomelen) / 2;
+			if (padding) {
+				abAppend(ab, "~", 1);
+				padding--;
+			}
+			while (padding--) abAppend(ab, " ", 1);
+
+			abAppend(ab, welcome, welcomelen);
+		} 
+		else { // Line start symbol
+			abAppend(ab, "~", 1);
+		}
+
+		// Clear following? line per refresh 
+		abAppend(ab, "\x1b[K", 3);
+
+		// New line
 		if (y < E.screenrows - 1) {
-			write(STDOUT_FILENO, "\r\n", 2);
+			abAppend(ab, "\r\n", 2);
 		}
 	}
 }
 
 void editorRefreshScreen(void) {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	struct abuf ab = ABUF_INIT;
+	
+	abAppend(&ab, "\x1b[?25l", 6);
+	// abAppend(&ab, "\x1b[2J", 4);
+	abAppend(&ab, "\x1b[H", 3);
 
-	editorDrawRows();
+	editorDrawRows(&ab);
 
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
+
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
 }
 
 /*** INPUT ***/
